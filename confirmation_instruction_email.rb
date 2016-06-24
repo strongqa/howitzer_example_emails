53a4f70e84e41f7c968a2cfbff4bd0fbe3035e0a
Roman Parashchenko <romikoops1@gmail.com>
2016-06-24 21:21:56 +0200

    refactored emails to use pages

--- confirmation_instruction_email.rb
+++ confirmation_instruction_email.rb
@@ -2,12 +2,16 @@ class ConfirmationInstructionEmail < Howitzer::Email
   subject "Confirmation instructions"
 
   def confirm_my_account
-    Howitzer::Web::Page.visit confirmation_link
+    ConfirmationEmailPage.open(token: token)
   end
 
   def confirmation_link
     res = plain_text_body[/Confirm my account \((.+?)\)/, 1]
     log.error("Confirmation link was not found") if res.nil?
     res
+  end
+
+  def token
+    confirmation_link[/confirmation_token=(.+)/, 1]
   end
 end
--- reset _password_confirmation_email.rb
+++ reset _password_confirmation_email.rb
@@ -2,12 +2,16 @@ class ResetPasswordConfirmationEmail < Howitzer::Email
   subject "Reset password instructions"
 
   def confirm_my_account
-    Howitzer::Web::Page.visit confirmation_link
+    ChangePasswordPage.open(token: token)
   end
 
   def confirmation_link
     res = plain_text_body[/Change my password \((.+?)\)/, 1]
     log.error("Confirmation link was not found") if res.nil?
     res
+  end
+
+  def token
+    confirmation_link[/reset_password_token=(.+)/, 1]
   end
 end
